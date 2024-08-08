2ec0298c03cd8f75cacb3e0e2543008deff4e14a
Nestlir <Cassandreray@gmail.com>
2024-08-08 19:51:17 +0300

    Туду

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,42 +1,78 @@
-import { FC, useMemo } from 'react';
+import { FC, useMemo, useEffect } from 'react';
 import { TConstructorIngredient } from '@utils-types';
 import { BurgerConstructorUI } from '@ui';
+import { useDispatch, useSelector } from '../../services/store';
+import { useNavigate } from 'react-router-dom';
+
+import {
+  burgerConstructorActions,
+  burgerConstructorSelectors
+} from '../../services/slices/burger-constructor-slice';
+import {
+  fetchOrderBurger,
+  orderActions,
+  orderSelectors
+} from '../../services/slices/orderSlice';
+import { fetchUser, userSelectors } from '../../services/slices/userSlice';
 
 export const BurgerConstructor: FC = () => {
-  /** TODO: взять переменные constructorItems, orderRequest и orderModalData из стора */
-  const constructorItems = {
-    bun: {
-      price: 0
-    },
-    ingredients: []
-  };
+  // Hooks
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const orderRequest = false;
-
-  const orderModalData = null;
-
-  const onOrderClick = () => {
-    if (!constructorItems.bun || orderRequest) return;
-  };
-  const closeOrderModal = () => {};
-
-  const price = useMemo(
-    () =>
-      (constructorItems.bun ? constructorItems.bun.price * 2 : 0) +
-      constructorItems.ingredients.reduce(
-        (s: number, v: TConstructorIngredient) => s + v.price,
-        0
-      ),
-    [constructorItems]
+  // Selectors
+  const user = useSelector(userSelectors.userSelector);
+  const bun = useSelector(burgerConstructorSelectors.bunSelector);
+  const ingredients = useSelector(
+    burgerConstructorSelectors.ingredientsSelector
   );
+  const orderRequest = useSelector(orderSelectors.isLoadingSelector);
+  const orderModalData = useSelector(orderSelectors.orderSelector);
 
-  return null;
+  // Effects
+  useEffect(() => {
+    if (!user) {
+      dispatch(fetchUser());
+    }
+  }, [dispatch, user]);
 
+  // Handlers
+  const onOrderClick = () => {
+    if (!bun || orderRequest) return;
+
+    if (!user) {
+      return navigate('/login');
+    }
+
+    const bunId = bun._id;
+    const ingredientsIds = ingredients.map((item) => item._id);
+    const orderData = [bunId, ...ingredientsIds, bunId];
+
+    dispatch(fetchOrderBurger(orderData));
+  };
+
+  const closeOrderModal = () => {
+    dispatch(orderActions.clearOrderModalDataAction());
+    dispatch(burgerConstructorActions.clearIngredients());
+  };
+
+  // Calculations
+  const calculatePrice = useMemo(() => {
+    const bunPrice = bun ? bun.price * 2 : 0;
+    const ingredientsPrice = ingredients.reduce(
+      (total: number, ingredient: TConstructorIngredient) =>
+        total + ingredient.price,
+      0
+    );
+    return bunPrice + ingredientsPrice;
+  }, [bun, ingredients]);
+
+  // Render
   return (
     <BurgerConstructorUI
-      price={price}
+      price={calculatePrice}
       orderRequest={orderRequest}
-      constructorItems={constructorItems}
+      constructorItems={{ bun, ingredients }}
       orderModalData={orderModalData}
       onOrderClick={onOrderClick}
       closeOrderModal={closeOrderModal}
--- src/components/ingredients-category/ingredients-category.tsx
+++ src/components/ingredients-category/ingredients-category.tsx
@@ -2,29 +2,33 @@ import { forwardRef, useMemo } from 'react';
 import { TIngredientsCategoryProps } from './type';
 import { TIngredient } from '@utils-types';
 import { IngredientsCategoryUI } from '../ui/ingredients-category';
+import { useSelector } from '../../services/store';
+import { burgerConstructorSelectors } from '../../services/slices/burger-constructor-slice';
 
 export const IngredientsCategory = forwardRef<
   HTMLUListElement,
   TIngredientsCategoryProps
 >(({ title, titleRef, ingredients }, ref) => {
-  /** TODO: взять переменную из стора */
-  const burgerConstructor = {
-    bun: {
-      _id: ''
-    },
-    ingredients: []
-  };
+  // Использование селекторов для получения булочки и ингредиентов отдельно
+  const bun = useSelector(burgerConstructorSelectors.bunSelector);
+  const constructorIngredients = useSelector(
+    burgerConstructorSelectors.ingredientsSelector
+  );
 
   const ingredientsCounters = useMemo(() => {
-    const { bun, ingredients } = burgerConstructor;
     const counters: { [key: string]: number } = {};
-    ingredients.forEach((ingredient: TIngredient) => {
+
+    // Подсчет количества каждого ингредиента
+    constructorIngredients.forEach((ingredient: TIngredient) => {
       if (!counters[ingredient._id]) counters[ingredient._id] = 0;
       counters[ingredient._id]++;
     });
+
+    // Если булочка добавлена, добавляем её в счетчик
     if (bun) counters[bun._id] = 2;
+
     return counters;
-  }, [burgerConstructor]);
+  }, [bun, constructorIngredients]);
 
   return (
     <IngredientsCategoryUI
